694ba3a39f6b6f6d3d676a6dca4604e8cfceba1c
bjcworth <bjcworth92@gmail.com>
2014-03-27 15:35:12 -0400

    added files

--- Player.java
+++ Player.java
@@ -1,234 +1,111 @@
-/**  
- * Player class.
- * 
- * 
- * 
- * code implemented by: Alex Brewer & Brandon Charlesworth
- * 
- * This code calculates the score of each space on the board, depending on the number
- * of adjacent player-occupied spaces, and accounts for a few special cases. It finds the
- * space with the maximum score and returns that space to move. With the exception of
- * a few clever tatics, it will block most efforts to get four in a row. It gives 
- * priority to blocking the opponent over getting a win itself, which is playing it
- * on the safe(defensive) side. End.
- **/
+/*  Sample Player class which just makes a random move
+ */
+
 
 import java.util.Random; 
-
 public class Player {
-
-    private final int height = 8;
-    private final int width = 8;
-
-    //calls whereIsMax
-    public Pair move(int[][] board) {
-        while(true) {
-            return whereIsMax(board, 2);
-        }  
-}
-    /**
-     * This move function is for the extra credit.
-     */
-    public Pair move(int[][] board, int player) {
-        while(true) {
-            return whereIsMax(board, player);
-        }  
-}
-
-    //Increments a score object based on the strategic value of the position
-    public int findScore(Pair p, int[][] board, int player) {
-        int score = 0;
-        if(p.row < 5 && p.col < 5) {
-            if(board[p.row + 1][p.col + 1] == (player % 2) + 1 && board[p.row + 2][p.col + 2] == (player % 2) + 1 && board[p.row + 3][p.col + 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.row > 2 && p.col > 2) {
-            if(board[p.row - 1][p.col - 1] == (player % 2) + 1 && board[p.row - 2][p.col - 2] == (player % 2) + 1 && board[p.row - 3][p.col - 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.row < 5 && p.col > 2) {
-            if(board[p.row + 1][p.col - 1] == (player % 2) + 1 && board[p.row + 2][p.col - 2] == (player % 2) + 1 && board[p.row + 3][p.col - 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.row > 2 && p.col < 5) {
-            if(board[p.row - 1][p.col + 1] == (player % 2) + 1 && board[p.row - 2][p.col + 2] == (player % 2) + 1 && board[p.row - 3][p.col + 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.row < 5) {
-            if(board[p.row + 1][p.col] == (player % 2) + 1 && board[p.row + 2][p.col] == (player % 2) + 1 && board[p.row + 3][p.col] == (player % 2) + 1) score += 200;
-        }
-        if(p.row > 2) {
-            if(board[p.row - 1][p.col] == (player % 2) + 1 && board[p.row - 2][p.col] == (player % 2) + 1 && board[p.row - 3][p.col] == (player % 2) + 1) score += 200;
-        }
-        if(p.col < 5) {
-            if(board[p.row][p.col + 1] == (player % 2) + 1 && board[p.row][p.col + 2] == (player % 2) + 1 && board[p.row][p.col + 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.col > 2) {
-            if(board[p.row][p.col - 1] == (player % 2) + 1 && board[p.row][p.col - 2] == (player % 2) + 1 && board[p.row][p.col - 3] == (player % 2) + 1) score += 200;
-        }
-        if(p.row < 6 && p.col < 6) {
-            if(board[p.row + 1][p.col + 1] == (player % 2) + 1 && board[p.row + 2][p.col + 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.row > 1 && p.col > 1) {
-            if(board[p.row - 1][p.col - 1] == (player % 2) + 1 && board[p.row - 2][p.col - 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.row < 6 && p.col > 1) {
-            if(board[p.row + 1][p.col - 1] == (player % 2) + 1 && board[p.row + 2][p.col - 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.row > 1 && p.col < 6) {
-            if(board[p.row - 1][p.col + 1] == (player % 2) + 1 && board[p.row - 2][p.col + 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.row < 6) {
-            if(board[p.row + 1][p.col] == (player % 2) + 1 && board[p.row + 2][p.col] == (player % 2) + 1) score += 20;
-        }
-        if(p.row > 1) {
-            if(board[p.row - 1][p.col] == (player % 2) + 1 && board[p.row - 2][p.col] == (player % 2) + 1) score += 20;
-        }
-        if(p.col < 6) {
-            if(board[p.row][p.col + 1] == (player % 2) + 1 && board[p.row][p.col + 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.col > 1) {
-            if(board[p.row][p.col - 1] == (player % 2) + 1 && board[p.row][p.col - 2] == (player % 2) + 1) score += 20;
-        }
-        if(p.row < 7 && p.col < 7) {
-            if(board[p.row + 1][p.col + 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.row > 0 && p.col > 0) {
-            if(board[p.row - 1][p.col - 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.row < 7 && p.col > 0) {
-            if(board[p.row + 1][p.col - 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.row > 0 && p.col < 7) {
-            if(board[p.row - 1][p.col + 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.row < 7) {
-            if(board[p.row + 1][p.col] == (player % 2) + 1) score += 2;
-        }
-        if(p.row > 0) {
-            if(board[p.row - 1][p.col] == (player % 2) + 1) score += 2;
-        }
-        if(p.col < 7) {
-            if(board[p.row][p.col + 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.col > 0) {
-            if(board[p.row][p.col - 1] == (player % 2) + 1) score += 2;
-        }
-        if(p.row < 5 && p.col < 5) {
-            if(board[p.row + 1][p.col + 1] == player && board[p.row + 2][p.col + 2] == player && board[p.row + 3][p.col + 3] == player) score += 250;
-        }
-        if(p.row > 2 && p.col > 2) {
-            if(board[p.row - 1][p.col - 1] == player && board[p.row - 2][p.col - 2] == player && board[p.row - 3][p.col - 3] == player) score += 250;
-        }
-        if(p.row < 5 && p.col > 2) {
-            if(board[p.row + 1][p.col - 1] == player && board[p.row + 2][p.col - 2] == player && board[p.row + 3][p.col - 3] == player) score += 250;
-        }
-        if(p.row > 2 && p.col < 5) {
-            if(board[p.row - 1][p.col + 1] == player && board[p.row - 2][p.col + 2] == player && board[p.row - 3][p.col + 3] == player) score += 250;
-        }
-        if(p.row < 5) {
-            if(board[p.row + 1][p.col] == player && board[p.row + 2][p.col] == player && board[p.row + 3][p.col] == player) score += 250;
-        }
-        if(p.row > 2) {
-            if(board[p.row - 1][p.col] == player && board[p.row - 2][p.col] == player && board[p.row - 3][p.col] == player) score += 250;
-        }
-        if(p.col < 5) {
-            if(board[p.row][p.col + 1] == player && board[p.row][p.col + 2] == player && board[p.row][p.col + 3] == player) score += 250;
-        }
-        if(p.col > 2) {
-            if(board[p.row][p.col - 1] == player && board[p.row][p.col - 2] == player && board[p.row][p.col - 3] == player) score += 250;
-        }
-        if(p.row < 6 && p.col < 6) {
-            if(board[p.row + 1][p.col + 1] == player && board[p.row + 2][p.col + 2] == player) score += 10;
-        }
-        if(p.row > 1 && p.col > 1) {
-            if(board[p.row - 1][p.col - 1] == player && board[p.row - 2][p.col - 2] == player) score += 10;
-        }
-        if(p.row < 6 && p.col > 1) {
-            if(board[p.row + 1][p.col - 1] == player && board[p.row + 2][p.col - 2] == player) score += 10;
-        }
-        if(p.row > 1 && p.col < 6) {
-            if(board[p.row - 1][p.col + 1] == player && board[p.row - 2][p.col + 2] == player) score += 10;
-        }
-        if(p.row < 6) {
-            if(board[p.row + 1][p.col] == player && board[p.row + 2][p.col] == player) score += 10;
-        }
-        if(p.row > 1) {
-            if(board[p.row - 1][p.col] == player && board[p.row - 2][p.col] == player) score += 10;
-        }
-        if(p.col < 6) {
-            if(board[p.row][p.col + 1] == player && board[p.row][p.col + 2] == player) score += 10;
-        }
-        if(p.col > 1) {
-            if(board[p.row][p.col - 1] == player && board[p.row][p.col - 2] == player) score += 10;
-        }
-        if(p.row < 7 && p.col < 7) {
-            if(board[p.row + 1][p.col + 1] == player) score += 1;
-        }
-        if(p.row > 0 && p.col > 0) {
-            if(board[p.row - 1][p.col - 1] == player) score += 1;
-        }
-        if(p.row < 7 && p.col > 0) {
-            if(board[p.row + 1][p.col - 1] == player) score += 1;
-        }
-        if(p.row > 0 && p.col < 7) {
-            if(board[p.row - 1][p.col + 1] == player) score += 1;
-        }
-        if(p.row < 7) {
-            if(board[p.row + 1][p.col] == player) score += 1;
-        }
-        if(p.row > 0) {
-            if(board[p.row - 1][p.col] == player) score += 1;
-        }
-        if(p.col < 7) {
-            if(board[p.row][p.col + 1] == player) score += 1;
-        }
-        if(p.col > 0) {
-            if(board[p.row][p.col - 1] == player) score += 1;
-        }
-        //special cases
-        // diagonals
-        if(p.row > 1 && p.row < 7 && p.col > 1 && p.col < 7) {
-          if(board[p.row-2][p.col-2] == 1 && board[p.row - 1][p.col - 1] == (player % 2) + 1 && board[p.row + 1][p.col + 1] == (player % 2) + 1) score +=25;
-        }
-        if(p.row > 1 && p.row < 7 && p.col > 0 && p.col < 6) {
-          if(board[p.row-2][p.col+2] == 1 && board[p.row - 1][p.col + 1] == (player % 2) + 1 && board[p.row + 1][p.col - 1] == (player % 2) + 1) score +=25;
-        }
-        if(p.row > 0 && p.row < 6 && p.col > 1 && p.col < 7) {
-          if(board[p.row+2][p.col-2] == 1 && board[p.row + 1][p.col - 1] == (player % 2) + 1 && board[p.row - 1][p.col + 1] == (player % 2) + 1) score +=25;
-        }
-        if(p.row > 0 && p.row < 6 && p.col > 0 && p.col < 6) {
-          if(board[p.row+2][p.col+2] == 1 && board[p.row - 1][p.col + 1] == (player % 2) + 1 && board[p.row + 1][p.col - 1] == (player % 2) + 1) score +=25;
-        }
-        
-        // verticals
-        if(p.row > 0 && p.row < 6) {
-          if(board[p.row - 1][p.col] == (player % 2) + 1 && board[p.row + 1][p.col] == (player % 2) + 1 && board[p.row + 2][p.col] == (player % 2) + 1) score += 25;  
-        }
-        
-        if(p.row > 1 && p.row < 7) {
-          if(board[p.row - 1][p.col] == (player % 2) + 1 && board[p.row + 1][p.col] == (player % 2) + 1 && board[p.row - 2][p.col] == (player % 2) + 1) score += 25;
-        }
-        
-        // horizontals
-        if(p.col > 0 && p.col < 6) {
-          if(board[p.row][p.col - 1] == (player % 2) + 1 && board[p.row][p.col + 1] == (player % 2) + 1 && board[p.row][p.col + 2] == (player % 2) + 1) score +=25;
-        }
-        if(p.col > 1 && p.col < 7) {
-          if(board[p.row][p.col - 1] == (player % 2) + 1 && board[p.row][p.col + 1] == (player % 2) + 1 && board[p.row][p.col - 2] == (player % 2) + 1) score +=25;
-        }
-        return score;
-    }
+  private static Random ran = new Random();
+  static final int height = 8;          
+  static final int width = 8;
+  private static int [][] slot = new int[height][width];                 // 0 = no piece; 1 = player;  2 = machine
+ 
     
-    //finds the value of each space on the board and returns the space with the largest value
-    public Pair whereIsMax(int[][] board, int player) {
-        Pair max = new Pair(0,0);
-        for(int i = 0; i < height; i++) 
-            for(int j = 0; j < width; j++) {
-                Pair newMax = new Pair(i, j);
-                if(board[newMax.row][newMax.col] == 0) {
-                    if(findScore(newMax, board, player) >= findScore(max, board, player)) {
-                        max = newMax;
-                    }
-                }
-            }
-        return max;
+ public Pair move(int[][] board) {
+  while(true) {
+
+  
+ //if(board[row][col] == 0) {
+ return trackPlayer(1);
+  }
+ }
+      
+  
+  
+  private static boolean rowOfThree(int [][] board, int player, 
+                                int i, int j, int rowInc, int colInc) { 
+      for(int k = 0; k <= 3; ++k) {
+        if(board[i][j] != player)
+          return false;
+        i += rowInc;
+        j += colInc;   
+      }
+      return true; 
+   }
+  
+  private Pair trackPlayer(int player) {    
+  int row = 0;
+  int col = 0;
+
+    // check all horizontal slots
+    for(int i = 0; i < height; ++i)
+      for(int j = 0; j < width-4; ++j) {
+         if(rowOfThree(slot, player , i, j, 0, 1)) {
+           int j2 = j + 1;
+           if(slot[i][j2] ==0) {
+            row = i;
+            col = j2;
+           }
+         }
+         else {
+      col = ran.nextInt(7);     // which slot to start search for a move
+      row = ran.nextInt(7); 
+         
+         } return new Pair (row, col);
+    } 
+  
+ 
+    // check all vertical slots
+    for(int i = 0; i < height-4; ++i)
+      for(int j = 0; j < width; ++j) {
+         if(rowOfThree(slot, player, i, j, 1, 0)) {
+           
+           //when found
+        int i2 = i + 1;
+        if(slot[i2][j] == 0) {
+     row = i2;
+       col = j;
         }
-    }
-            
+         }
+         else {
+      col = ran.nextInt(7);     // which slot to start search for a move
+      row = ran.nextInt(7); 
+         
+         } 
+    } return new Pair (row, col);
+
+/*
+   // check all lower-left to upper-right diagonals
+    for(int i = 3-1; i < height; ++i)
+      for(int j = 0; j < width-3+1; ++j) {
+      
+         if(rowOfThree(slot, player, i, j, -1, 1)) {
+             // code when found win
+           int i2 = i; int j2 = j; 
+           for(int k = 0; k < 3; ++k) {
+             slotButton[i2][j2].setBackground(win[player]); 
+             slotButton[i2][j2].setOpaque(true); 
+             i2 += -1; j2 += 1; 
+           }
+ 
+         }  
+    }   
+    // check all upper-left to lower-right diagonals
+    for(int i = 0; i < height-3+1; ++i)
+      for(int j = 0; j < width-3+1; ++j) {
+         if(rowOfThree(slot, player, i, j, 1, 1)) {
+           int i2 = i; int j2 = j; 
+           for(int k = 0; k < 3; ++k) {
+             slotButton[i2][j2].setBackground(win[player]); 
+             slotButton[i2][j2].setOpaque(true); 
+             i2 += 1; j2 += 1; 
+           }
+
+         }
+    } */
+
+  } 
+  
+  
+
+  
+}
